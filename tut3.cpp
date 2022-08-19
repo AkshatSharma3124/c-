03d7297bfbac7f297331248e40e5c8515f166d81
AkshatSharma3124 <sharmaakshat5600@gmail.com>
2022-08-19 21:06:09 +0530

    code added

---
+++ tut3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+// THIS IS MY FIRST COMMENT
+// This is my first multiline comment
+//    This is second line
+//    hello1
+//    hello2
+//    
+   
+
+int main(){
+    int sum=6;
+    cout<<"Hello World I am WRITING MY PROGRAM WITH MULTILINE AND SINGLE LINE COMMENTS"<< sum;
+
+    return 0;
+
+}
---
+++ tut4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+int glo=6;
+void sum(){
+    int a;
+    cout<<glo;
+
+}
+
+int main(){
+    int glo=9;
+    glo=78; 
+    int a=4;
+    int b=5;
+    float pi=3.14;
+    char c='u';
+    bool is_true=false;
+    sum();
+    cout<<glo<<is_true;
+
+    cout<<"This is tutorial 4"<<". Value of a is "<<a<<". Value of b is "<<b;
+    cout<<"\nThe value of pi is "<<pi;
+    cout<<"\nThe value of c is "<<c;
+
+
+    return 0;
+
+}
