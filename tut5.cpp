52750971a770aed3bd2362e51dc9134b95e29eeb
AkshatSharma3124 <sharmaakshat5600@gmail.com>
2022-08-21 00:45:22 +0530

    Create tut5.cpp

---
+++ tut5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+    cout<<"Enter the value of num1: \n"; //"<< THIS IS AN INSERTION OPERATOR"
+    cin>>num1;  //">> THIS IS AN EXTRACTION OPERATOR"
+    cout<<"Enter the value of num2: \n";
+    cin>>num2;
+
+    cout<<"The Sum is: "<<num1+num2;
+    
+    return 0;
+}
