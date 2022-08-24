3ecb21bc895129e44364c2d5551f6e4bb90ac28b
AkshatSharma3124 <sharmaakshat5600@gmail.com>
2022-08-24 23:06:19 +0530

    Create tut10.cpp

---
+++ tut10.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+// syntax for FOR loop 
+// for(initialization;condition;updation){
+// loop body(C++ Code)
+//}
+int main(){
+    for(int i=0; i<40; i++){
+        cout<<"\n"<<i;
+    /*syntax for while loop
+      while(condition){
+        C++ STATEMENTS
+      }*/
+    int a=1;
+    while(a<=40){
+        cout<<"\n"<<a;
+        a++;
+    }
+    }
+    /*Syntax for do while loop 
+      do{C++ STATEMENTS}
+      while(condition)*/
+    int j=1;
+    do{
+        cout<<"\n"<<j;
+        j++;
+    }while(j<=40);
+    return 0;
+    
+
+    
+}
